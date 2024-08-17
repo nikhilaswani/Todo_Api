dfa530b31f86a57c92462cdb6f25b29fc8fd26f4
“nikhilaswani” <nikhilaswani927@gmail.com>
2024-08-17 19:15:54 +0530

    change in db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://0.0.0.0:27017/mydatabase");
+    await mongoose.connect(process.env.MONGO_URI);
     // await mongoose.connect(process.env.MONGO_URI, {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
