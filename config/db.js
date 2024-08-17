c6113c823cd870ad614c6101358f41536e96876f
“nikhilaswani” <nikhilaswani927@gmail.com>
2024-08-17 19:06:01 +0530

    change in mongo uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://0.0.0.0:27017/mydatabase");
     // await mongoose.connect(process.env.MONGO_URI, {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
